f4ef98c88532b664bf6e72d90e3e7e2437c8832a
LMcIntosh99 <lsmcintosh99@gmail.com>
2025-06-25 22:02:32 +0100

    reset alembic

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,7 +3,7 @@ from .database import Base
 
 class Comment(Base):
     __tablename__ = "comments"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     text = Column(String)
     location = Column(String)
     created_at = Column(DateTime, server_default=func.now())
